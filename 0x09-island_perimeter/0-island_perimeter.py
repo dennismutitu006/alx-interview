11e482368a79b2748cc26d1f2476a2ac78736eac
dennismutitu <dennismutitu06@gmail.com>
2024-08-01 11:07:57 +0300

    perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''this function will find the perimeter of an island by
+navigating its grids rows and columns.
+'''
+def island_perimeter(grid):
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check top
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                # Check bottom
+                if i == rows-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                # Check left
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                # Check right
+                if j == cols-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+
+    return perimeter
